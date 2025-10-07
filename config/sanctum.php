2f6a97482571217366af46f55b99310b845b2b63
Passing01 <147635730+Passing01@users.noreply.github.com>
2025-10-07 16:45:22 +0000

    Déplacement du Dockerfile à la racine

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,11 +15,8 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+    'stateful' => array_filter(explode(',', env('SANCTUM_STATEFUL_DOMAINS', 
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,'.\Illuminate\Support\Str::after(env('APP_URL', 'http://localhost'), '://')
     ))),
 
     /*
@@ -80,5 +77,18 @@ return [
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Expiration Minutes
+    |--------------------------------------------------------------------------
+    |
+    | This value controls the number of minutes until an issued token will be
+    | considered expired. This will override any values set in the token's
+    | "expires_at" attribute, but first-party sessions are not affected.
+    |
+    */
+
+    'expiration' => 60 * 24 * 7, // 7 jours
 
 ];
